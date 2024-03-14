5603d50b6c6f5a92ffb63263e2d36000b3ccbd1d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-14 14:15:15 +0100

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,30 +14,27 @@ createApp({
         openChat(index) {
             this.currentContact = { name: this.contacts[index].name, avatar: this.contacts[index].avatar, messages: [...this.contacts[index].messages] };
         },
+        getDateAndTime() {
+            this.currentTime = luxon.DateTime.now();
+            return { date: this.currentTime.toFormat('dd/MM/yyyy'), time: this.currentTime.toFormat('HH:mm') };
+        },
         send(name) {
             // find current chat by name in contacts array
             const currentChatByName = contacts.find(contact => contact.name === name);
-            // get current time
-            this.currentTime = luxon.DateTime.now();
-            // format time and date
-            const date = this.currentTime.toFormat('dd/MM/yyyy');
-            const time = this.currentTime.toFormat('HH:mm');
+            // get current time and date
+            const dateAndTime = this.getDateAndTime();
             // create new message
-            let newMsg = { date: date, time: time, message: this.newMessage, status: 'sent' }
+            let newMsg = { date: dateAndTime.date, time: dateAndTime.time, message: this.newMessage, status: 'sent' };
             // update current and main contacts array
             currentChatByName.messages.push(newMsg);
             this.currentContact.messages.push(newMsg);
-
-            this.automaticAnswer(currentChatByName)
+            // trigger automatic answer
+            this.automaticAnswer(currentChatByName);
         },
         automaticAnswer(currentChatByName) {
             setTimeout(() => {
-                this.currentTime = luxon.DateTime.now();
-                // format time and date
-                const date = this.currentTime.toFormat('dd/MM/yyyy');
-                const time = this.currentTime.toFormat('HH:mm');
-                let newMsg = { date: date, time: time, message: 'ok', status: 'received' };
-                // update current and main contacts array
+                const dateAndTime = this.getDateAndTime();
+                let newMsg = { date: dateAndTime.date, time: dateAndTime.time, message: 'ok', status: 'received' };
                 currentChatByName.messages.push(newMsg);
                 this.currentContact.messages.push(newMsg);
             }, 1000);
