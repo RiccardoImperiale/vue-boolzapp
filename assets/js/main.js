c132a24eae03df711fbfbc7a33eb3fc1ea2878b6
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-14 14:00:30 +0100

    add send new message functionality and add current date and time using luxon library

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { contacts } from './contacts.js'
+// const { DateTime } = luxon;
 const { createApp } = Vue
 
 createApp({
@@ -6,14 +7,28 @@ createApp({
         return {
             contacts: contacts,
             currentContact: [],
+            newMessage: '',
+            currentTime: ''
         }
     },
     methods: {
         openChat(index) {
-            this.currentContact = { name: this.contacts[index].name, avatar: this.contacts[index].avatar, messages: [...this.contacts[index].messages] }
-            console.log(this.currentContact);
-            // const contact = this.contacts[index];
-            // this.currentContact = contact ? { name: contact.name, messages: [...contact.messages] } : null;
+            this.currentContact = { name: this.contacts[index].name, avatar: this.contacts[index].avatar, messages: [...this.contacts[index].messages] };
+        },
+        send(name) {
+            // find current chat by name in contacts array
+            const currentChatByName = contacts.find(contact => contact.name === name);
+            // get current time
+            this.currentTime = luxon.DateTime.now();
+            // format time and date
+            const date = this.currentTime.toFormat('dd/MM/yyyy');
+            const time = this.currentTime.toFormat('HH:mm');
+            // create new message
+            let newMsg = { date: date, time: time, message: this.newMessage, status: 'sent' }
+            // update main contacts array
+            currentChatByName.messages.push(newMsg);
+            // also show in chat by push in current chat array
+            this.currentContact.messages.push(newMsg);
         }
     },
     created() {
