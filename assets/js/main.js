8775385fade3083e5e4f322a1229669fca4c9055
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-14 10:52:12 +0100

    add click on single chat and get an object containing the single contact info only

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            contacts: contacts
+            contacts: contacts,
+            currentChat: null
         }
     },
     methods: {
-      
+        openChat(index){
+            this.currentChat = {name: this.contacts[index].name, messages: [...this.contacts[index].messages]}
+            console.log(this.currentChat.name);
+        }
     },
     created() {
         console.log(contacts);
